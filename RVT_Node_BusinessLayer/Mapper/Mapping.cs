48e6fef86ee65c7c357de9c54383bfe7adcfed2b
mrelectrq <tiora100bass@gmail.com>
2020-12-24 10:47:12 +0200

    Added Mapper for Node and NodeData model

---
+++ RVT_Node_BusinessLayer/Mapper/Mapping.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using RVT_Node_BusinessLayer.BusinessModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RVT_Node_BusinessLayer.Mapper
+{
+    public class Mapping
+    {
+        private static readonly Lazy<IMapper> _mapper = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.ShouldMapProperty = p => p.GetMethod.IsPublic || p.GetMethod.IsAssembly;
+                cfg.AddProfile<MappingProile>();
+            });
+
+            var mapper = config.CreateMapper();
+            return mapper;
+        });
+
+        public static IMapper Mapper => _mapper.Value;
+    }
+
+    internal class MappingProile : Profile
+    {
+        public MappingProile()
+        {
+            CreateMap<NodeConfig, Node>();
+        }
+    }
+}
